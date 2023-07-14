a7559decb2b6e500c5dfaf52c8d5713b72251337
bribkb <106629260+bribkb@users.noreply.github.com>
2023-07-14 12:53:03 -0400

    update

--- server.js
+++ server.js
@@ -12,10 +12,9 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-polished-31947',
-    user : 'postgres',
-    password : 'test',
-    database : 'smart-brain'
+    connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
   }
 });
 
